afdbc2871d482f3fb135f920ce3a41e7b6eef887
OleksandrKhokhlov <hohlovsanya@gmail.com>
2023-11-12 17:57:55 +0200

    remove pattern

--- shemas/contacts.js
+++ shemas/contacts.js
@@ -1,13 +1,9 @@
 const Joi = require("joi");
 
 const contactSchema = Joi.object({
-  name: Joi.string()
-    .pattern(/[A-Z][a-z]* [A-Z][a-z]*/)
-    .required(),
+  name: Joi.string().required(),
   email: Joi.string().email().required(),
-  phone: Joi.string()
-    .pattern(/\(\d{3}\) \d{3}-\d{4}/)
-    .required(),
+  phone: Joi.string().required(),
   favorite: Joi.boolean(),
 });
 
